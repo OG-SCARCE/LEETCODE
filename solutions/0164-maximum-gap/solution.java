642e5c8bffe2226b50c08ab7e6e1689aa6da320c
SCARCE <amanpatel.2805@gmail.com>
2026-06-07 17:49:15 +0000

    Sync LeetCode submission Runtime - 45 ms (59.45%), Memory - 90.9 MB (53.74%)

---
+++ solutions/0164-maximum-gap/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+
+        int maximumGap = 0;
+        int n = nums.length;
+
+        if (n == 1) {
+            return 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            maximumGap = Math.max(
+                maximumGap,
+                nums[i] - nums[i - 1]
+            );
+        }
+
+        return maximumGap;
+    }
+}
