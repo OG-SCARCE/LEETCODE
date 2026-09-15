402dec6cc0af72b9705c23e24a7f401947d16857
SCARCE <amanpatel.2805@gmail.com>
2026-09-15 08:32:54 +0000

    Sync LeetCode submission Runtime - 36 ms (5.03%), Memory - 54.2 MB (10.50%)

---
+++ solutions/1250-longest-common-subsequence/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int function(int i, int j, String s, String t, int[][] dp) {
+        if (i >= s.length() || j >= t.length()) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if (s.charAt(i) == t.charAt(j)) {
+            dp[i][j] = 1 + function(i + 1, j + 1, s, t, dp);
+        } else {
+            int a = function(i, j + 1, s, t, dp);
+            int b = function(i + 1, j, s, t, dp);
+
+            dp[i][j] = Math.max(a, b);
+        }
+
+        return dp[i][j];
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+
+        for (int i = 0; i < text1.length(); i++) {
+            java.util.Arrays.fill(dp[i], -1);
+        }
+
+        return function(0, 0, text1, text2, dp);
+    }
+}
